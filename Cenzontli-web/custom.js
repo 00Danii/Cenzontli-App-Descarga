fca7e853290c9d002f2943e8d1591068ec117299
Danii <jdaniel.om97@gmail.com>
2023-08-19 00:57:19 -0600

    Actualizacion

---
+++ Cenzontli-web/custom.js
@@ -0,0 +1,8 @@
+(function ($) {
+    "use strict";
+    //PRE LOADER
+    $(window).load(function () {
+        // 1600
+        $(".preloader").delay(1600).slideUp("slow"); // set duration in brackets
+    });
+})(jQuery);

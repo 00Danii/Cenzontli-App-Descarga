5e3042b68472b55ecf0a6d4d00246fd787d60c3b
Danii <jdaniel.om97@gmail.com>
2023-08-19 11:51:37 -0600

    Cambio de servidor

--- Cenzontli-web/custom.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    //PRE LOADER
-    $(window).load(function () {
-        // 1600
-        $(".preloader").delay(1600).slideUp("slow"); // set duration in brackets
-    });
-})(jQuery);
